6577a0f967e76b40c4782f5541c60191b2e42ed3
shreyansh goel <03shreyanshgoel@gmail.com>
2025-01-14 16:41:09 +0530

    updated code

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use("/api", productRoutes);
 app.use("/api", paymentRoutes);
 app.use("/api", orderRoutes);
 
-const port = env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Your server is running at port number: ${port}`);
